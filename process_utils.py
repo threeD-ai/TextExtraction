d29654f7bb27318d858f18ea53246cba812ffe53
Abhigyan <ramanabhigyan@gmail.com>
2020-07-18 06:36:47 +0530

    quick fixes and better organisation

--- process_utils.py
+++ process_utils.py
@@ -5,11 +5,16 @@ import cv2
 import math
 
 def loadImage(img_file):
-    img = io.imread(img_file)           # RGB order
-    if img.shape[0] == 2: img = img[0]
-    if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    if img.shape[2] == 4:   img = img[:,:,:3]
-    img = np.array(img)
+
+    if img_file[-3:]=='.tif':
+        image = cv2.imread(img_file)
+        img = image[:,:,::-1]
+    else:
+        img = io.imread(img_file)           # RGB order
+        if img.shape[0] == 2: img = img[0]
+        if len(img.shape) == 2 : img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        if img.shape[2] == 4:   img = img[:,:,:3]
+        img = np.array(img)
 
     return img
 
